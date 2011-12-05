90d21da24d853d079d74c744ac981beb9bd26f5c
Natan Yellin <aantny@gmail.com>
2011-12-05 10:10:19 +0200

    Configure LaTeX export for org-mode

--- home/.emacs.d/custom/personal/org-config.el
+++ home/.emacs.d/custom/personal/org-config.el
@@ -10,5 +10,38 @@
 ;; File locations
 ;; TODO: Fixme
 (org-agenda-files (quote ("c:/Users/natany/Desktop/projects.org")))
+(require 'org-latex)
+
+
+;; Latex export
+;; from http://orgmode.org/worg/org-tutorials/org-latex-export.html
+;; for prettier exports, see:
+;; - http://emacs-fu.blogspot.com/2011/04/nice-looking-pdfs-with-org-mode-and.html
+;; Export to PDF w/ "C-c C-e p"
+;; first you need to fix Emacs' PATH so that zsh will find pdflatex.
+;; See http://stackoverflow.com/questions/4393628/emacs-shell-command-not-found
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+
+(add-to-list 'org-export-latex-classes
+             `("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}"))
+             )
+(add-to-list 'org-export-latex-classes
+             `("natan-writing"
+               ,(concat
+		 "\\documentclass{article}"
+		 "\\linespread{1.2}" ;; TODO: only set this for the body
+		 "\\setlength{\\parskip}{1ex plus 0.5ex minus 0.2ex}")
+               ("\\section{%s}" . "\\section*{%s}")))
+
+;; create an org-mode link from my old links
+;; e.g. creates a link from the following
+;; link name
+;;    link
+;; note the indentation before link
+(fset 'turn-name-\\n-link-to-org-link
+   "Djjpk0xxxi[[\C-e]\C-f[\C-e]]jkkJxkJ")
 
 (provide 'personal/org-config)
